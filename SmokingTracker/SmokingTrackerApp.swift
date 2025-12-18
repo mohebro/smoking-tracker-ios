d24bef3162933482d7339609952f35b9f0b2c720
mohebro <mohebro@icloud.com>
2025-12-18 16:38:23 +0330

    Initial Commit

---
+++ SmokingTracker/SmokingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmokingTrackerApp.swift
+//  SmokingTracker
+//
+//  Created by mohebro on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmokingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
